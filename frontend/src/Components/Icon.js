32003db151695c0fecb061e0d3778926efd2fca4
shreejalmaharjan-27 <shreejal@rafled.com>
2024-12-02 17:11:36 +0000

    added icon component

---
+++ frontend/src/Components/Icon.js
@@ -0,0 +1,5 @@
+const Icon = ({ name }) => {
+    return <i className="material-symbols-outlined"> {name} </i>;
+};
+
+export default Icon;
---
+++ frontend/src/Pages/debug.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Icon from '../Components/Icon'
+
+export default function debug() {
+    return (
+        <>
+            <h1 className="text-4xl">Debug</h1>
+            <p className="text-lg">This is a debug page.</p>
+
+            <Icon name="help" />
+        </>
+    )
+}
