145e9a230173bc95e92c9e9e08483d8f29bd8828
Komronbek Olimov <116647964+komronbeko@users.noreply.github.com>
2024-12-03 13:24:09 +0000

    Query and Delete methods added to backend

--- backend/src/controllers/football.controller.js
+++ backend/src/controllers/football.controller.js
@@ -3,22 +3,8 @@ const Game = require("../schemas/Game.schema");
 const postData = async (req, res) => {
   try {
     const data = req.body;
-    console.log(data);
 
     Game.create(data);
-
-    res.status(200).json({ message: "Success" });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
-
-const updateData = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const data = req.body;
-
-    await Game.findByIdAndUpdate(id, data);
 
     res.status(200).json({ message: "Success" });
   } catch (error) {
@@ -35,4 +21,155 @@ const getData = async (req, res) => {
   }
 };
 
-module.exports = { getData, postData, updateData };
+const updateData = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+
+    const findOne = await Game.findById(id);
+
+    if (!findOne) return res.status(403).json({ message: "Game Not Found" });
+
+    await Game.findByIdAndUpdate(id, data);
+
+    res.status(200).json({ message: "Success" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const deletedata = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const findOne = await Game.findById(id);
+
+    if (!findOne) return res.status(403).json({ message: "Game Not Found" });
+
+    await Game.findByIdAndDelete(id);
+
+    res.status(200).json({ message: "Success" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const getYearlyStats = async (req, res) => {
+  try {
+    const { year } = req.params;
+
+    const stats = await Game.aggregate([
+      {
+        $match: {
+          year: +year,
+        },
+      },
+      {
+        $group: {
+          _id: "$year",
+          totalGamesPlayed: {
+            $sum: "$games_played",
+          },
+          totalDraws: {
+            $sum: "$draw",
+          },
+          totalWins: {
+            $sum: "$win",
+          },
+        },
+      },
+    ]);
+
+    res.status(400).json({
+      message: "success",
+      data: stats.length
+        ? stats[0]
+        : { total_games_played: 0, win: 0, draw: 0 },
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const getWins = async (req, res) => {
+  try {
+    const { input } = req.query;
+
+    const data = await Game.aggregate([
+      {
+        $match: {
+          win: {
+            $gt: +input,
+          },
+        },
+      },
+      {
+        $limit: 10,
+      },
+    ]);
+
+    res.status(400).json({ message: "success", data });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const getAvgGoals = async (req, res) => {
+  try {
+    const { input } = req.query;
+
+    const data = await Game.aggregate([
+      {
+        $match: {
+          year: +input,
+        },
+      },
+      {
+        $group: {
+          _id: "$team",
+          avg_goals_for: {
+            $avg: "$goals_for",
+          },
+          loss: {
+            $first: "$loss",
+          },
+          goals_against: {
+            $first: "$goals_against",
+          },
+          year: {
+            $first: "$year",
+          },
+          __v: {
+            $first: "$__v",
+          },
+          games_played: {
+            $first: "$games_played",
+          },
+          draw: {
+            $first: "$draw",
+          },
+          win: {
+            $first: "$win",
+          },
+          points: {
+            $first: "$points",
+          },
+        },
+      },
+    ]);
+
+    res.status(400).json({ message: "success", data });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getData,
+  postData,
+  updateData,
+  deletedata,
+  getYearlyStats,
+  getWins,
+  getAvgGoals,
+};
--- backend/src/routes/football.routes.js
+++ backend/src/routes/football.routes.js
@@ -3,12 +3,20 @@ const {
   getData,
   postData,
   updateData,
+  deletedata,
+  getYearlyStats,
+  getWins,
+  getAvgGoals,
 } = require("../controllers/Football.controller");
 
 const router = Router();
 
-router.put("/update/:id", updateData);
 router.post("/add", postData);
 router.get("/", getData);
+router.put("/update/:id", updateData);
+router.delete("/delete/:id", deletedata);
+router.get("/get_stats/:year", getYearlyStats);
+router.get("/get_wins", getWins);
+router.get("/get_avg_goals", getAvgGoals);
 
 module.exports = router;
