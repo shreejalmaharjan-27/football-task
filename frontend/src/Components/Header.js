fe24ff90813d6f0b9341920acb6722dd242b6107
shreejalmaharjan-27 <shreejal@rafled.com>
2024-12-04 10:50:29 +0000

    add link to homepage

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
         <header className='w-full text-yellow p-4 bg-darkBlack'>
             <div className="container mx-auto">
-                <h1 className="text-1xl font-bold font-display">FOOTBALL MANAGER</h1>
+                <Link to="/"><h1 className="text-1xl font-bold font-display">FOOTBALL MANAGER</h1></Link>
             </div>
         </header>
     )
