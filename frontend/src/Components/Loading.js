d5324de56c62bfca99946dad90c5d9583d42fd3d
shreejalmaharjan-27 <shreejal@rafled.com>
2024-12-04 15:19:46 +0000

    create loading component

---
+++ frontend/src/Components/Loading.js
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className="w-full h-full flex items-center justify-center border border-gray-300 rounded-2xl p-6">
+            <p className="text-lg font-bold">Loading...</p>
+        </div>
+    )
+};
+
+export default Loading;
