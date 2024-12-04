8ad74b7d76752aaa3156c4f8d274450bab7a3c22
Chris <gopalicrisostam@gmail.com>
2024-12-04 00:58:46 +0000

    Added new styling (dark) to header, footer and layout

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -1,9 +1,18 @@
 const Footer = () => {
-    return (
-        <footer className="w-full bg-slate-700 p-8 mt-6 flex items-center justify-center text-white">
-            <p>Copyright 2024, <a href="https://github.com/shreejalmaharjan-27/football-task/blob/main/README.md" target="_blank" rel="noreferrer">Team</a></p>
-        </footer>
-    );
-}
+  return (
+    <footer className="w-full bg-darkBlack p-8 mt-6 flex items-center justify-center text-yellow ">
+      <p>
+        Copyright 2024,{" "}
+        <a
+          href="https://github.com/shreejalmaharjan-27/football-task/blob/main/README.md"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Team
+        </a>
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
