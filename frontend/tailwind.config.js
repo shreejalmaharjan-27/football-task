1f772749cc01686e1f243c4876c40d661185f426
shreejalmaharjan-27 <shreejal@rafled.com>
2024-12-04 10:42:08 +0000

    revert changes from tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,21 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit", //for faster runtime
+  darkMode: "class",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
       display: ['"Roboto"', '"sans-serif"'], //need to add a new font
-    },
-
-    extend: {
-      colors: {
-        black: "#161616", // Background black
-        yellow: "#FFEB3B", // Bright yellow for accents
-        white: "#FFFFFF", // Primary text
-        gray: "#B0BEC5", // Cool gray for secondary text
-        red: "#E53935", // Rich red for alerts or urgency
-        darkBlack: "#121212", // Optional for card backgrounds
-      },
     },
   },
   plugins: [],
