d8af54dc30b4d00a995960702845911a5fd3a743
Chris <gopalicrisostam@gmail.com>
2024-12-04 00:59:56 +0000

    added custom colors and font to tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  mode: "jit", //for faster runtime
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['"Roboto"', '"sans-serif"'], //need to add a new font
+    },
+
+    extend: {
+      colors: {
+        black: "#161616", // Background black
+        yellow: "#FFEB3B", // Bright yellow for accents
+        white: "#FFFFFF", // Primary text
+        gray: "#B0BEC5", // Cool gray for secondary text
+        red: "#E53935", // Rich red for alerts or urgency
+        darkBlack: "#121212", // Optional for card backgrounds
+      },
+    },
   },
   plugins: [],
-}
-
+};
