7c9b9afc332dc90cd59f7f1120e69735f034ec57
Chris <gopalicrisostam@gmail.com>
2024-12-05 14:04:04 +0000

    homepage hover effect

--- frontend/src/Components/HomeCard.js
+++ frontend/src/Components/HomeCard.js
@@ -6,21 +6,21 @@ const HomeCard = (props) => {
   const { title, description, link, bgwrapper, icon_name } = props;
 
   return (
-    <div className="w-80">
+    <div className="w-80 transform transition-all duration-500 hover:scale-105 relative ">
       <Link to={link} className="hover:no-underline">
         <div
-          className="bg-cover bg-center h-60 w-80 rounded-lg p-0 m-0 relative overflow-hidden"
+          className="bg-cover bg-center h-60 w-80 rounded-lg p-0 m-0 relative overflow-hidden "
           style={{ backgroundImage: `url('${bgwrapper}')` }}
         >
-          <div className="absolute inset-0 bg-gradient-to-t from-yellow from-0% to-50%">
+          <div className="absolute inset-0 bg-gradient-to-t from-[#FFEB3B] from-0% to-5% hover:to-100% l ">
             <div className="absolute inset-x-0 bottom-0 rounded-lg ">
-              <div className="leading-none p-6 pb-3 rounded-2xl mt-auto mb-2 text-2xl font-semibold tracking-tight text-black">
+              <div className="leading-none p-6 pb-2 rounded-2xl mt-auto mb-2 text-2xl font-semibold tracking-tight text-white ">
                 {title}
                 <span className="">
                   <Icon name={icon_name} />
                 </span>
                 <br />
-                <span className=" text-sm">{description}</span>
+                <span className=" text-sm text-white ">{description}</span>
               </div>
             </div>
           </div>
