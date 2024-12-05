f17664b0ad62662deb56932f1864e24ea712817c
shreejalmaharjan-27 <shreejal@rafled.com>
2024-12-05 14:21:02 +0000

    complete teams page

--- frontend/src/Pages/Teams.js
+++ frontend/src/Pages/Teams.js
@@ -1,8 +1,36 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import Axios from '../Plugins/Axios';
+import useTitle from '../Components/SetTitle';
+const allTeams = async () => {
+    const request = await Axios.get('/all_teams');
+    return request.data.data;
+}
 const Teams = () => {
+
+    const [teams, setTeams] = useState([]);
+    useTitle('Teams');
+
+    useEffect(() => {
+        allTeams().then((data) => {
+            setTeams(data);
+        });
+    }, []);
+
     return (
-        <div>Team</div>
+        <div>
+            <h2 className="text-2xl font-bold text-white">Teams</h2>
+            <div className='flex flex-col '>
+                <ul>
+                    {
+                        teams.map((team, index) => (
+                            <li key={index} className='text-white p-2 border-b border-gray-600'>
+                                <h3>{team}</h3>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </div>
     )
 }
 
