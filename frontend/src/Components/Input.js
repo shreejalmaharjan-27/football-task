33111fac847c9694afa4492b73f0e58bc714d593
shreejalmaharjan-27 <shreejal@rafled.com>
2024-12-05 14:20:21 +0000

    fix typo, update components

--- frontend/src/Components/Input.js
+++ frontend/src/Components/Input.js
@@ -1,14 +1,18 @@
 const Input = (props) => {
+    const { label, id, name, className, readonly, ...extraProps } = props;
     return (
         <div className="inline-flex flex-col">
             {
-                props.label && (
-                    <label className="text-sm font-bold" htmlFor={props.id || props.name}>{props.label}</label>
+                label && (
+                    <label className="text-sm font-bold" htmlFor={id || name}>{label}</label>
                 )
             }
             <input
-                className={`${props.className} border border-gray-400 rounded-lg px-2 py-1 text-lg text-black`}
-                {...props}
+                readOnly={readonly}
+                id={id || name}
+                name={name}
+                className={`${className} border border-gray-400 rounded-lg px-2 py-1 text-lg text-black`}
+                {...extraProps}
             />
         </div>
     )
