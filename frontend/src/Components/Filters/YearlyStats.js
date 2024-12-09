ca4eb02bac2f1896341a54b800068573b3b14325
shreejalmaharjan-27 <shreejal@rafled.com>
2024-12-09 14:38:06 +0000

    use unique values on yearly stats

--- frontend/src/Components/Filters/YearlyStats.js
+++ frontend/src/Components/Filters/YearlyStats.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Button from '../Button'
 import Input from '../Input'
+import Select from '../Select'
 
-export default function YearlyStats({ text, setText, submit }) {
+export default function YearlyStats({ text, setText, submit, options }) {
     return (
         <div className='flex flex-col'>
-            <Input label="Get Yearly Stats" type="number" value={text} onChange={(e) => setText(e.target.value)} placeholder="Year" required />
+            {/* <Input label="Get Yearly Stats" type="number" value={text} onChange={(e) => setText(e.target.value)} placeholder="Year" required /> */}
+            <Select value={text} onChange={(e) => setText(e.target.value)}>
+
+                <option value="" disabled>Select Year</option>
+                {options.map((option, index) => <option key={index} value={option}>{option}</option>)}
+            </Select>
             <Button onClick={submit}>Submit</Button>
         </div>
     )
